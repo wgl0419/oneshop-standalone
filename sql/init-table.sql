8107ba36738bb025799adc0f3af3ab134ece2aaf
Elvin Zeng <zenghui@188.com>
2017-06-13 00:27:15 +0800

    重新初始化项目

--- sql/init-table.sql
+++ sql/init-table.sql
@@ -1,5 +1,5 @@
 --
--- desc：shoppingmall数据库表结构初始化脚本
+-- desc：oneshop(standalone version)数据库表结构初始化脚本
 -- Author:elvin
 -- Date: 2017-05-22
 --
---
+++ src/main/java/com/gelvt/OneshopApplication.java
@@ -0,0 +1,12 @@
+package com.gelvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneshopApplication.class, args);
+	}
+}
