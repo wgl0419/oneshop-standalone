a7c1442fd75eed7dfd12d7e3151c8642cd0f1ea4
Elvin Zeng <zenghui@188.com>
2017-06-20 23:19:27 +0800

    创建用户表的初始化sql脚本

--- sql/init-table.sql
+++ sql/init-table.sql
@@ -1,7 +1,30 @@
 --
 -- desc：oneshop(standalone version)数据库表结构初始化脚本
 -- Author:elvin
--- Date: 2017-05-22
+-- Date: 2017-06-20
 --
 
-
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `mobileNo` varchar(16) DEFAULT NULL,
+  `isActive` bit(1) NOT NULL,
+  `isDeleted` bit(1) NOT NULL,
+  `isEmailAuthenticated` bit(1) NOT NULL,
+  `isMobileNoAuthenticated` bit(1) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `regTime` datetime NOT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  `expireTime` datetime DEFAULT NULL,
+  `regIp` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_username` (`username`) USING BTREE,
+  UNIQUE KEY `uk_mobileno` (`mobileNo`) USING BTREE,
+  UNIQUE KEY `uk_email` (`email`) USING BTREE
+);
