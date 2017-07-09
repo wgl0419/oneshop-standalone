763549d42733fb0ed1cf8e4f7ff342a46ee33717
Elvin Zeng <zenghui@188.com>
2017-07-09 17:43:27 +0800

    修正几个mybatis整合的bug

--- oneshop/src/main/java/com/gelvt/oneshop/OneshopApplication.java
+++ oneshop/src/main/java/com/gelvt/oneshop/OneshopApplication.java
@@ -1,9 +1,11 @@
 package com.gelvt.oneshop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.gelvt.oneshop.mapper")
 public class OneshopApplication {
 
     public static void main(String[] args) {
