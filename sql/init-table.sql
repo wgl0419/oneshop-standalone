43ca98020d64fc7f8b2258b19540d91773e0f0d1
Elvin Zeng <zenghui@188.com>
2017-07-09 14:31:26 +0800

    创建用户系统相关的表

--- sql/init-table.sql
+++ sql/init-table.sql
@@ -1,30 +1,60 @@
 --
--- desc：oneshop(standalone version)数据库表结构初始化脚本
+-- 数据库表结构初始化脚本
 -- Author: Elvin Zeng
 -- Date: 2017-06-20
 --
 
--- ----------------------------
--- Table structure for tb_user
--- ----------------------------
+
+DROP TABLE IF EXISTS `tb_permission`;
+create table `tb_permission` (
+  id bigint auto_increment primary key,
+  name varchar(128) not null comment '权限名',
+  remark varchar(255) null comment '权限备注',
+  constraint tb_permission_name_uindex unique (name)
+)
+comment '权限表';
+
+DROP TABLE IF EXISTS `tb_role`;
+create table `tb_role` (
+  id bigint auto_increment primary key,
+  name varchar(64) not null comment '角色名',
+  remark varchar(255) null comment '角色备注',
+  constraint tb_role_name_uindex unique (name)
+)
+comment '角色表';
+
+DROP TABLE IF EXISTS `tb_role_permission`;
+create table `tb_role_permission` (
+  role_id bigint not null comment '角色id',
+  permission_id bigint not null comment '权限id',
+  constraint tb_role_permission_role_id_permission_id_uk
+  unique (role_id, permission_id)
+)
+;
+
 DROP TABLE IF EXISTS `tb_user`;
 CREATE TABLE `tb_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) DEFAULT NULL,
-  `email` varchar(128) DEFAULT NULL,
-  `mobileNo` varchar(16) DEFAULT NULL,
-  `isActive` bit(1) NOT NULL,
-  `isDeleted` bit(1) NOT NULL,
-  `isEmailAuthenticated` bit(1) NOT NULL,
-  `isMobileNoAuthenticated` bit(1) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `salt` varchar(255) DEFAULT NULL,
-  `regTime` datetime NOT NULL,
-  `updateTime` datetime DEFAULT NULL,
-  `expireTime` datetime DEFAULT NULL,
-  `regIp` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_username` (`username`) USING BTREE,
-  UNIQUE KEY `uk_mobileno` (`mobileNo`) USING BTREE,
-  UNIQUE KEY `uk_email` (`email`) USING BTREE
+  id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(64) COMMENT '用户名',
+  email VARCHAR(128) COMMENT '邮箱',
+  mobile_no VARCHAR(16) COMMENT '手机号',
+  is_email_verified TINYINT UNSIGNED DEFAULT 0 NOT NULL COMMENT '邮箱是否已经被验证',
+  is_mobile_no_verified TINYINT UNSIGNED DEFAULT 0 NOT NULL COMMENT '用户的手机号码是否已经被验证',
+  passwd VARCHAR(255) COMMENT '密码',
+  salt VARCHAR(255) COMMENT '盐',
+  is_disabled TINYINT UNSIGNED DEFAULT 0 NOT NULL COMMENT '账户是否被禁用',
+  reg_ip VARCHAR(255) COMMENT '帐号注册时候的ip地址',
+  gtm_create DATETIME DEFAULT NOW() NOT NULL COMMENT '创建时间',
+  gtm_modified DATETIME COMMENT '更新时间',
+  is_deleted TINYINT UNSIGNED DEFAULT 0 NOT NULL COMMENT '逻辑删除标志'
 );
+ALTER TABLE tb_user COMMENT = '用户表';
+
+DROP TABLE IF EXISTS `tb_user_role`;
+create table `tb_user_role`(
+  user_id bigint not null,
+  role_id bigint not null,
+  constraint tb_user_role_user_id_role_id_pk
+  unique (user_id, role_id)
+)
+;
